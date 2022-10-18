eca3f0ff02b77547501eca5921833ad16ddfe8e7
TangoCash <eric@loxat.de>
2022-10-18 10:25:16 +0200

    fix prepare / fix url

--- prepare.sh
+++ prepare.sh
@@ -4,3 +4,14 @@ sudo apt install freeglut3-dev
 sudo apt install libsigc++-2.0-dev
 sudo apt install libgif-dev
 sudo apt install libusb-1.0-0-dev
+
+sudo apt install libfreetype-dev
+sudo apt install libxml2-dev
+sudo apt install libswresample-dev
+sudo apt install libavformat-dev
+sudo apt install libswscale-dev
+sudo apt install libopenthreads-dev
+sudo apt install libjpeg-dev
+sudo apt install libfribidi-dev
+
+sudo apt install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
