0103edc6d2b89909d5a6a23e3996db90029c1a90
KittyBua <103779060+KittyBua@users.noreply.github.com>
2025-04-23 21:16:15 +0200

    Update archives.mk

--- make/archives.mk
+++ make/archives.mk
@@ -1,7 +1,7 @@
 # 
 LUA_VER=5.2.4
 LUAPOSIX_VER=31
-LIBDVBSI_VER=ff57e58
+LIBDVBSI_VER=0.3.9
 
 # luaposix: posix bindings for lua
 $(ARCHIVE)/luaposix-v$(LUAPOSIX_VER).tar.gz:
@@ -12,5 +12,5 @@ $(ARCHIVE)/lua-$(LUA_VER).tar.gz:
 	$(WGET) http://www.lua.org/ftp/lua-$(LUA_VER).tar.gz
 
 # libdvbsi
-$(ARCHIVE)/libdvbsi-git-$(LIBDVBSI_VER).tar.bz2:
-	$(SCRIPTS)/get-git-archive.sh https://github.com/opendreambox/opendreambox/tree/c1f99fa2f98548b017acad2da9d8511b03f4c564/meta-opendreambox/recipes-multimedia/libdvbsi%2B%2B/git/obi/libdvbsi++.git $(LIBDVBSI_VER) $(notdir $@) $(ARCHIVE)
+$(ARCHIVE)/libdvbsi-$(LIBDVBSI_VER).tar.bz2:
+	$(SCRIPTS)/get-git-archive.sh https://github.com/mtdcr/libdvbsi/releases/download/$(LIBDVBSI_VERSION) $(notdir $@) $(ARCHIVE)
